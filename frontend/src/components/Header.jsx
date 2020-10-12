91ba3a19c5fb2be3d4f06ebff7f243bd3e4e3030
AndriyPolukhin <andriy.polukhin@gmail.com>
2020-10-12 14:49:15 +0300

    added Ratings and ProductScreen, implemented Routing with react-router-dom

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
-import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">ProShop</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>ProShop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link href="/cart" className="fas fa-shopping-cart">
-                Cart
-              </Nav.Link>
-              <Nav.Link href="/login" className="fas fa-user">
-                Sign In
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link className="fas fa-shopping-cart">Cart</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link className="fas fa-user">Sign In</Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
